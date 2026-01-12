e77e5776a9acd10bc51ac8d3ab2f7cb2482b172a
mphomosia194 <mphomosia194@gmail.com>
2026-01-12 10:47:03 +0000

    Task 3: print numbers from 0 to 9

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+
+#endif

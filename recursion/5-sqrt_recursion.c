8d2d378883d41e65fd8bfd4571c1fe6afa0d95bd
mphomosia194 <mphomosia194@gmail.com>
2026-01-14 11:11:43 +0000

    Task 5: compute natural square root using recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - finds the natural square root recursively
+ * @n: number to find the square root of
+ * @guess: current guess
+ *
+ * Return: natural square root, or -1 if none exists
+ */
+int _sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to compute the square root of
+ *
+ * Return: natural square root, or -1 if none exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, 0));
+}

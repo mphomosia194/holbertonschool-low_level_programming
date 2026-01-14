e8959d90a49e1920676b453b47558d6699fd2a32
mphomosia194 <mphomosia194@gmail.com>
2026-01-14 14:23:51 +0000

    Fix size_t definition in function_pointers header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,31 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to a function that prints a name
+ */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array of integers
+ * @size: size of the array
+ * @action: pointer to the function to execute
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: array of integers
+ * @size: number of elements
+ * @cmp: comparison function
+ *
+ * Return: index or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* FUNCTION_POINTERS_H */

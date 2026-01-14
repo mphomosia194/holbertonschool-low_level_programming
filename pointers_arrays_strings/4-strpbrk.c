98f09a0859e00a2e52cc0168c6a8bf3d0934e2d4
mphomosia194 <mphomosia194@gmail.com>
2026-01-14 10:35:01 +0000

    Task 4: implement _strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to be scanned
+ * @accept: string containing the bytes to look for
+ *
+ * Return: pointer to the first occurrence in s of any byte
+ *         from accept, or NULL if no byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+
+	return (0);
+}

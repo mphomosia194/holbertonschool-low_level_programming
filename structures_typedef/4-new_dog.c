22eb94ef6d7ae2bc606954a7b5d6b125e17821ba
mphomosia194 <mphomosia194@gmail.com>
2026-01-14 13:58:54 +0000

    Refactor new_dog to satisfy Betty line limit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
@@ -12,51 +49,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_copy;
-	char *owner_copy;
-	int i, len_name, len_owner;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
-	len_name = 0;
-	while (name[len_name])
-		len_name++;
-
-	len_owner = 0;
-	while (owner[len_owner])
-		len_owner++;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(sizeof(char) * (len_name + 1));
-	if (name_copy == NULL)
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	owner_copy = malloc(sizeof(char) * (len_owner + 1));
-	if (owner_copy == NULL)
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog->owner == NULL)
 	{
-		free(name_copy);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
 
-	for (i = 0; i < len_name; i++)
-		name_copy[i] = name[i];
-	name_copy[i] = '\0';
-
-	for (i = 0; i < len_owner; i++)
-		owner_copy[i] = owner[i];
-	owner_copy[i] = '\0';
-
-	dog->name = name_copy;
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
 	dog->age = age;
-	dog->owner = owner_copy;
 
 	return (dog);
 }
